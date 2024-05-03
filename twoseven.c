157421b778beae325c95206d257ccf273641d335
Smasia <sander130506@gmail.com>
2024-05-03 13:47:39 +0200

    Added chapter 2.7 with finished code

---
+++ twoseven.c
@@ -0,0 +1,12 @@
+// Secure C Programming
+#include <stdio.h>
+
+// Function main begins program execution
+int main( void )
+{
+  printf( "Welcome to C!\n" );
+  puts( "Welcome to C!");
+
+  printf( "Welcome " );
+  printf( "%s", "Welcome " );
+}
